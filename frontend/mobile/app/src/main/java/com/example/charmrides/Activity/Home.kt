1995f1bf132547f45224e998694acc248741ad7f
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-15 08:04:48 +0530

    doneAllFragsUser

---
+++ frontend/mobile/app/src/main/java/com/example/charmrides/Activity/Home.kt
@@ -0,0 +1,12 @@
+package com.example.charmrides.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.charmrides.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
