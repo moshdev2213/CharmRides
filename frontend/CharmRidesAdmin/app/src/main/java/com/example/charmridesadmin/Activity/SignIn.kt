a2ec6c195df67bc2cfb0488559ff4939adaaf36d
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-16 20:43:42 +0530

    addedInspector

--- frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/SignIn.kt
+++ frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/SignIn.kt
@@ -1,11 +1,12 @@
-package com.example.charmridesadmin
+package com.example.charmridesadmin.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.charmridesadmin.R
 
-class MainActivity : AppCompatActivity() {
+class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
     }
 }
