3a5e1b2f9724edcaabafb3fef7beab6fbd094e03
nisal yapa <99547060+IT21298912@users.noreply.github.com>
2023-10-17 00:32:15 +0530

    bviewe
    done

--- frontend/Web/Web/src/components/Booking.vue
+++ frontend/Web/Web/src/components/Booking.vue
@@ -1,28 +1,28 @@
 <template>
     <div className="body-wrapper ">
-      <div className="container-fluid pt-4 pb-0">
-        <div className="row">
-          <div className="col-lg-12 d-flex align-items-stretch">
-            <div className="card w-100">
-              <div className="card-body p-3">
-                <div className="mt-3 d-flex justify-content-between align-items-baseline ">
-                  <h5 className="card-title fw-semibold mb-4">Bookings</h5>
-                  <input type="text" placeholder="Search" className="form-control w-auto" />
+        <div className="container-fluid pt-4 pb-0">
+            <div className="row">
+                <div className="col-lg-12 d-flex align-items-stretch">
+                    <div className="card w-100">
+                        <div className="card-body p-3">
+                            <div className="mt-3 d-flex justify-content-between align-items-baseline ">
+                                <h5 className="card-title fw-semibold mb-4">Bookings</h5>
+                                <input type="text" placeholder="Search" className="form-control w-auto" />
+                            </div>
+
+                            <div className="table-responsive">
+                                <DataTables searchQuery="{searchQuery}" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-  
-                <div className="table-responsive">
-                  <DataTables searchQuery="{searchQuery}" />
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
     <div className="body-wrapper ">
-      <div className="container-fluid pt-4 pb-0">
-        <div class="row">
-          <!-- Earnings (Monthly) Card Example
+        <div className="container-fluid pt-4 pb-0">
+            <div class="row">
+                <!-- Earnings (Monthly) Card Example
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
@@ -39,9 +39,9 @@
                                 </div>
                             </div>
                         </div> -->
-  
-          <!-- Earnings (Annual) Card Example -->
-          <!-- <div class="col-xl-3 col-md-6 mb-4">
+
+                <!-- Earnings (Annual) Card Example -->
+                <!-- <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-success shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -57,9 +57,9 @@
                                 </div>
                             </div>
                         </div> -->
-  
-          <!-- Tasks Card Example -->
-          <!-- <div class="col-xl-3 col-md-6 mb-4">
+
+                <!-- Tasks Card Example -->
+                <!-- <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-info shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -86,9 +86,9 @@
                                 </div>
                             </div>
                         </div> -->
-  
-          <!-- Pending Requests Card Example -->
-          <!-- <div class="col-xl-3 col-md-6 mb-4">
+
+                <!-- Pending Requests Card Example -->
+                <!-- <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-warning shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -104,74 +104,77 @@
                                 </div>
                             </div>
                         </div> -->
-        </div>
-      </div>
-  
-      <div className="container-fluid pt-4 pb-0">
-        <div class="row">
-          <div class="col-12">
-            <!-- modal of delivery starts -->
-            <div class="modal fade" style="z-index: 99999" id="modal-leave" tabindex="-1" role="dialog"
-              aria-labelledby="modal-form" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered modal-md" role="document">
-                <div class="modal-content">
-                  <div class="modal-body p-0">
-                    <div class="card card-plain">
-                      <div class="card-header pb-0 text-left">
-                        <h4 class="font-weight-bolder text-primary">Leaves</h4>
-                        <p class="mb-0">Enter Relevant Credentials</p>
-                      </div>
-                      <div class="card-body p-3">
-                        <form role="form text-left">
-                          <label>Coupon Name</label>
-                          <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Email" aria-label="Email"
-                              aria-describedby="email-addon" onfocus="focused(this)" onfocusout="defocused(this)" />
-                          </div>
-                          <label>Value</label>
-                          <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Password" aria-label="Password"
-                              aria-describedby="password-addon" onfocus="focused(this)" onfocusout="defocused(this)" />
-                          </div>
-  
-                          <div class="row text-center">
-                            <div class="col-6">
-                              <button type="button" data-bs-dismiss="modal"
-                                class="btn bg-gradient-primary btn-md w-100 mt-2 mb-0">
-                                Cancel
-                              </button>
-                            </div>
-                            <div class="col-6">
-                              <button type="submit" class="btn btn-outline-primary btn-md w-100 mt-2 mb-0">
-                                Add
-                              </button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-            <!-- modal of delivery  Ends -->
-            <div class="card">
-              <div class="card-header pb-0 p-3">
-                <div class="row">
-                  <div class="col-6 d-flex align-items-center">
-                    <h6 class="mb-0">Booking List</h6>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body p-3">
-                <div class="d-sm-flex justify-content-between p-0">
-                  <!-- <div>
+        </div>
+
+        <div className="container-fluid pt-4 pb-0">
+            <div class="row">
+                <div class="col-12">
+                    <!-- modal of delivery starts -->
+                    <div class="modal fade" style="z-index: 99999" id="modal-leave" tabindex="-1" role="dialog"
+                        aria-labelledby="modal-form" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                            <div class="modal-content">
+                                <div class="modal-body p-0">
+                                    <div class="card card-plain">
+                                        <div class="card-header pb-0 text-left">
+                                            <h4 class="font-weight-bolder text-primary">Leaves</h4>
+                                            <p class="mb-0">Enter Relevant Credentials</p>
+                                        </div>
+                                        <div class="card-body p-3">
+                                            <form role="form text-left">
+                                                <label>Coupon Name</label>
+                                                <div class="input-group mb-3">
+                                                    <input type="text" class="form-control" placeholder="Email"
+                                                        aria-label="Email" aria-describedby="email-addon"
+                                                        onfocus="focused(this)" onfocusout="defocused(this)" />
+                                                </div>
+                                                <label>Value</label>
+                                                <div class="input-group mb-3">
+                                                    <input type="text" class="form-control" placeholder="Password"
+                                                        aria-label="Password" aria-describedby="password-addon"
+                                                        onfocus="focused(this)" onfocusout="defocused(this)" />
+                                                </div>
+
+                                                <div class="row text-center">
+                                                    <div class="col-6">
+                                                        <button type="button" data-bs-dismiss="modal"
+                                                            class="btn bg-gradient-primary btn-md w-100 mt-2 mb-0">
+                                                            Cancel
+                                                        </button>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <button type="submit"
+                                                            class="btn btn-outline-primary btn-md w-100 mt-2 mb-0">
+                                                            Add
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- modal of delivery  Ends -->
+                    <div class="card">
+                        <div class="card-header pb-0 p-3">
+                            <div class="row">
+                                <div class="col-6 d-flex align-items-center">
+                                    <h6 class="mb-0">Booking List</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body p-3">
+                            <div class="d-sm-flex justify-content-between p-0">
+                                <!-- <div>
                                             <a href="javascript:;" class="btn btn-icon btn-outline-dark">
                                                 New order
                                             </a>
                                         </div> -->
-                  <div class="d-flex">
-                    <!-- <div class="dropdown d-inline">
+                                <div class="d-flex">
+                                    <!-- <div class="dropdown d-inline">
                       <a href="javascript:;" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown"
                         id="navbarDropdownMenuLink2">
                         Filters
@@ -195,65 +198,66 @@
                         </li>
                       </ul>
                     </div> -->
-  
-                    <!-- <router-link to="/addIns" class="btn btn-icon btn-outline-dark ms-2 export">
+
+                                    <!-- <router-link to="/addIns" class="btn btn-icon btn-outline-dark ms-2 export">
                       <span class="btn-inner--icon"><i class="ni ni-archive-2"></i></span>
                       <span class="btn-inner--text">Booking Listr</span>
                     </router-link> -->
-  
-                    <button class="btn btn-icon btn-outline-dark ms-2 export" @click="exportToCSV" type="button">
-                      <span class="btn-inner--icon"><i class="ni ni-archive-2"></i></span>
-                      <span class="btn-inner--text">Export CSV</span>
-                    </button>
-                  </div>
-                </div>
-                <div class="table-responsive" id="tbl_exporttable_to_xls_4">
-                    <table class="table table-flush" id="datatable-basic2">
-              <thead class="thead-light">
-                <tr>
-                  <th>Booking_Id</th>
-                  <th>User_Id</th>
 
-                  <th>Vehicle_Number</th>
-                  <th>Booking_Date</th>
-                 
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="booking in Bookings" :key="booking.bid">
-                  <td>
-                    <div class="d-flex align-items-center">
-                      <p class="text-xs font-weight-bold ms-2 mb-0">
-                        {{ booking.bid }}
-                      </p>
+                                    <button class="btn btn-icon btn-outline-dark ms-2 export" @click="exportToCSV"
+                                        type="button">
+                                        <span class="btn-inner--icon"><i class="ni ni-archive-2"></i></span>
+                                        <span class="btn-inner--text">Export CSV</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="table-responsive" id="tbl_exporttable_to_xls_4">
+                                <table class="table table-flush" id="datatable-basic2">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th>Booking_Id</th>
+                                            <th>User_Id</th>
+
+                                            <th>Vehicle_Number</th>
+                                            <th>Booking_Date</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="booking in Bookings" :key="booking.bid">
+                                            <td>
+                                                <div class="d-flex align-items-center">
+                                                    <p class="text-xs font-weight-bold ms-2 mb-0">
+                                                        {{ booking.bid }}
+                                                    </p>
+                                                </div>
+                                            </td>
+                                            <td class="font-weight-bold">
+                                                <span class="my-2 text-xs">{{ booking.uid }}</span>
+                                            </td>
+                                            <td class="font-weight-bold">
+                                                <span class="my-2 text-xs">{{ booking.vid }}</span>
+                                            </td>
+                                            <td class="font-weight-bold">
+                                                <span class="my-2 text-xs">{{ booking.date }}</span>
+                                            </td>
+
+
+
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <!-- modal of delivery starts -->
+
+                                <!-- modal of delivery  Ends -->
+                            </div>
+                        </div>
                     </div>
-                  </td>
-                  <td class="font-weight-bold">
-                    <span class="my-2 text-xs">{{ booking.uid }}</span>
-                  </td>
-                  <td class="font-weight-bold">
-                    <span class="my-2 text-xs">{{ booking.vid }}</span>
-                  </td>
-                  <td class="font-weight-bold">
-                    <span class="my-2 text-xs">{{ booking.date }}</span>
-                  </td>
-       
-        
- 
-                </tr>
-              </tbody>
-            </table>
-                  <!-- modal of delivery starts -->
-                 
-                  <!-- modal of delivery  Ends -->
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </template>
+</template>
 
 <script>
 import BookingService from "../services/BookingService";
@@ -262,34 +266,34 @@ import * as XLSX from "xlsx";
 import Swal from "sweetalert2";
 
 export default {
-  name: "Bookinglist",
+    name: "Bookinglist",
 
-  data() {
-    return {
-      Bookings: [],
-    };
-  },
-
-  methods: {
-    getBookings() {
-      BookingService.getBookings().then((response) => {
-        this.Bookings = response.data;
-      });
+    data() {
+        return {
+            Bookings: [],
+        };
     },
 
-    exportToCSV() {
-  const Bookings = this.Bookings;
-  const ws = XLSX.utils.json_to_sheet(Bookings);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Bookings');
-  XLSX.writeFile(wb, 'Bookings.csv');
-},
-  },
+    methods: {
+        getBookings() {
+            BookingService.getBookings().then((response) => {
+                this.Bookings = response.data;
+            });
+        },
 
-  created() {
-    this.getBookings();
-    setInterval(this.getBookings, 1000);
-  },
+        exportToCSV() {
+            const Bookings = this.Bookings;
+            const ws = XLSX.utils.json_to_sheet(Bookings);
+            const wb = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(wb, ws, 'Bookings');
+            XLSX.writeFile(wb, 'Bookings.csv');
+        },
+    },
+
+    created() {
+        this.getBookings();
+        setInterval(this.getBookings, 1000);
+    },
 };
 </script>
 
