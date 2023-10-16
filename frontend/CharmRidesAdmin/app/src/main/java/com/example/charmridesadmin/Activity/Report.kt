bd5145583ef82e0941893b494dfe9028a01097ac
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-16 22:31:14 +0530

    AddedActivities

---
+++ frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/Report.kt
@@ -0,0 +1,12 @@
+package com.example.charmridesadmin.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.charmridesadmin.R
+
+class Report : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
