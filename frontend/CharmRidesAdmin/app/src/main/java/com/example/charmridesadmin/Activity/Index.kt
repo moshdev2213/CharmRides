ce91e9e9b2b74ed4114d33c2193df64035925c50
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-21 00:14:35 +0530

    fragInitAndQrScanDone

--- frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/Index.kt
+++ frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/Index.kt
@@ -2,11 +2,44 @@ package com.example.charmridesadmin.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.charmridesadmin.Fragment.IndexFragment
+import com.example.charmridesadmin.Fragment.ProfileFragment
+import com.example.charmridesadmin.Fragment.ReportFragment
 import com.example.charmridesadmin.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Index : AppCompatActivity() {
+    private lateinit var bottomNavigationView:BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        replaceFrag(IndexFragment())
+        bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.profile->replaceFrag(ProfileFragment())
+                R.id.homie->replaceFrag(IndexFragment())
+                R.id.report->replaceFrag(ReportFragment())
+                else->{
+
+                }
+            }
+            true
+        }
+    }
+    private fun replaceFrag(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+//        if(userObj !=null){
+//            val bundle = Bundle()
+//            bundle.putSerializable("user",userObj)
+//            fragment.arguments = bundle
+//        }
+
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
--- frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/SignIn.kt
+++ frontend/CharmRidesAdmin/app/src/main/java/com/example/charmridesadmin/Activity/SignIn.kt
@@ -1,12 +1,34 @@
 package com.example.charmridesadmin.Activity
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.charmridesadmin.R
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        askPermissionForCamera()
+        askPermissionForInternet()
     }
+    private fun askPermissionForCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 101)
+            }
+        }
+    }
+    private fun askPermissionForInternet() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.INTERNET), 102)
+            }
+        }
+    }
+
 }
